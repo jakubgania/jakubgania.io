430061858a915ee092986f872f3d160d2ec0a879
jakubgania <jakubgania@outlook.com>
2019-06-02 13:21:55 +0200

    initial version of the main page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-content>
-      <v-container>
+      <v-container fluid style="padding: 0px;">
         <nuxt />
       </v-container>
     </v-content>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,27 @@
 <template>
-  <v-layout>
-    index page
-  </v-layout>
+  <div class="flex-center position-ref full-height container-index">
+    <div class="content">
+      <div class="main-title">
+        {{ mainTitle }}
+      </div>
+      <div class="my-name">
+        {{ myName }}
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      mainTitle: 'Full Stack Web Developer',
+      myName: 'Jakub Gania'
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+  @import './assets/scss/index/lg.scss';
+</style>
