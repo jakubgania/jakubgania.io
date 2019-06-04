9a49dbed43b5ee3968fd79da526e1918aa97fef5
jakubgania <jakubgania@outlook.com>
2019-06-04 23:31:45 +0200

    create about subpage and about scss folder

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container-about">
+    <div class="content">
+      <div class="information-section">
+        Information section
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+  @import './assets/scss/about/lg.scss';
+</style>
