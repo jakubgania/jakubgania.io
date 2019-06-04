d6c3685153e258c67564b215993ab2cf13d5b5e9
jakubgania <jakubgania@outlook.com>
2019-06-04 23:37:49 +0200

    update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -2,11 +2,45 @@
   <div class="container-about">
     <div class="content">
       <div class="information-section">
+        <v-layout row wrap>
+          <v-flex lg4>
+            img
+          </v-flex>
+          <v-flex lg8>
+            fullname
+          </v-flex>
+        </v-layout>
         Information section
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      info: ''
+    }
+  },
+  head() {
+    return {
+      title: '',
+      meta: [
+        {
+          hid: '',
+          name: '',
+          content: ''
+        },
+        {
+          name: '',
+          content: ''
+        }
+      ]
+    }
+  }
+}
+</script>
 
 <style lang="sass" scoped>
   @import './assets/scss/about/lg.scss';
