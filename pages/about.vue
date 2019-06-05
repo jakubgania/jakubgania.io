c9a0d38d6df75f95d845ecad1a9547172d1a7d26
jakubgania <jakubgania@outlook.com>
2019-06-06 00:22:17 +0200

    update about.vue subpage

--- pages/about.vue
+++ pages/about.vue
@@ -4,13 +4,28 @@
       <div class="information-section">
         <v-layout row wrap>
           <v-flex lg4>
-            img
+            <div class="image-section">
+              <v-avatar :size="photoSize">
+                <img :src="profileImage" alt="Jakub Gania - profile photo">
+              </v-avatar>
+            </div>
           </v-flex>
-          <v-flex lg8>
-            {{ fullname }}
+          <v-flex lg8 class="description-name-section">
+            <div class="description-name">
+              {{ fullname }}
+            </div>
+          </v-flex>
+          <v-flex lg12 style="padding-top: 20px;">
+            <div class="description">
+              {{ description1 }}
+            </div>
+          </v-flex>
+          <v-flex lg12 style="padding-top: 20px;">
+            <div class="description">
+              {{ description2 }}
+            </div>
           </v-flex>
         </v-layout>
-        Information section
       </div>
     </div>
   </div>
@@ -20,7 +35,22 @@
 export default {
   data() {
     return {
-      fullname: 'Jakub Gania'
+      profileImage: '',
+      fullname: 'Jakub Gania',
+      description1: 'Nazywam się Jakub Gania. Mieszkam we Wrocławiu. W 2018 roku uzyskałem tytuł inżyniera informatki z specjalizacją inżyniera systemów informatycznych. Specjalizuję sie w technologiach webowych. Cały czas się uczę i poszerzam swoją wiedzę w różnych obszarach powiązanych z programowaniem. Moje projekty udostępniam na platformie GitHub. Rozwijam się w kierunku Full Stack Web Developer.',
+      description2: 'Prywatnie interesują mnie nowe technologie, startupy, rozwój osobisty i bardzo lubię fotografować.'
+    }
+  },
+  computed: {
+    photoSize() {
+      switch (this.$vuetify.breakpoint.name) {
+        case 'xs': return '120'
+        case 'sm': return '120'
+        case 'md': return '120'
+        case 'lg': return '140'
+        case 'xl': return '160'
+        default: return '160'
+      }
     }
   },
   head() {
