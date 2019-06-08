ed38224217b08f4907837c9daf835a1f9540fc8f
jakubgania <jakubgania@outlook.com>
2019-06-08 23:16:32 +0200

    create new subpage - contact and new folder contact in assets path

---
+++ pages/contact.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-layout row wrap>
+    <v-flex xs12 sm12 md12 lg8 offset-lg2>
+      <div class="title-page">
+        Kontakt
+      </div>
+    </v-flex>
+
+    <v-flex xs12 sm12 md12 lg8 offset-lg2>
+      <v-chip class="email-tag">
+        <v-icon style="margin-right:14px;">
+          alternate_email
+        </v-icon>
+        <a href="mailto:contact@jakubgania.io" class="email-link">contact@jakubgania.io</a>
+      </v-chip>
+    </v-flex>
+  </v-layout>
+</template>
+
+<style lang="scss" scoped>
+  @import './assets/scss/contact/lg.scss';
+</style>
