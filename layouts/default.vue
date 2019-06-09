d0d99e00378956550a10af986ec656c9187bfce2
jakubgania <jakubgania@outlook.com>
2019-06-09 19:09:44 +0200

    update devault.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,17 @@
 <template>
-  <v-app>
-    <v-content>
-      <toolbar-header-component @switchNavigationDrawer="switchNavigationDrawer"/>
-      <v-container fluid style="padding: 0px;">
-        <nuxt />
-      </v-container>
-    </v-content>
-  </v-app>
+    <v-app>
+        <v-content>
+            <toolbar-header-component @switchNavigationDrawer="switchNavigationDrawer"/>
+            <v-container fluid style="padding: 0px;">
+                <nuxt />
+            </v-container>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
 import ToolbarHeader from '../components/toolbar-header.vue';
+
 export default {
 	components: {
 		'toolbar-header-component': ToolbarHeader,
