0f2b7e81ce76fb244e2e06ac4ca6c1aff286b41a
jakubgania <jakubgania@outlook.com>
2019-06-19 19:27:54 +0200

    update projects subpage

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,11 +1,32 @@
 <template>
     <v-layout row wrap>
-        projects subpage
-    </v-layout>
+    <div class="container">
+      <v-layout row wrap>
+        <v-flex v-for="project in model.projects" :key="project.id" lg12>
+          <div class="links-section">
+            <nuxt-link :to="'/projects/' + project.path" class="link">
+              {{ project.title }}
+            </nuxt-link>
+          </div>
+        </v-flex>
+      </v-layout>
+    </div>
+  </v-layout>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
+    async asyncData({params}) {
+        try {
+            let {data} = await axios.get(`http://192.168.0.19:3000/projects-list/all`)
+
+            return {model: data}
+        } catch (error) {
+            return {model: 'Taki post nie istnieje.', error: true}
+        }
+    },
     data() {
         return {
             title: 'projekty'
