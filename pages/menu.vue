874c85d0be63f4bf619ec5da43261d8fc1ed4a00
jakubgania <jakubgania@outlook.com>
2019-09-16 23:38:23 +0200

    implementation menu subpage

---
+++ pages/menu.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-row class="content-container">
+    <v-col v-for="item in links" :key="item.id" lg="12">
+      {{ item.title }}
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        {
+          title: 'O mnie',
+          path: '/about'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.content-container {
+  max-width: 800px;
+  width: 100%;
+  background-color: azure;
+  margin: auto;
+}
+</style>
