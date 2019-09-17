d76f8edbee1ce09f201270b63614b670e4e85b44
jakubgania <jakubgania@outlook.com>
2019-09-18 01:07:20 +0200

    added about subpage

---
+++ pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-row class="content-container">
+    about me
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      //
+    }
+  }
+}
+</script>
+
+<style scoped>
+.content-container {
+  /*  */
+}
+</style>
