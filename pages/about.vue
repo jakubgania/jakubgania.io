8f6dfa65c478de0fd961cafa5a2dc4144329faec
jakubgania <jakubgania@outlook.com>
2019-09-19 18:35:19 +0200

    implemented about subpage

--- pages/about.vue
+++ pages/about.vue
@@ -1,27 +1,53 @@
 <template>
   <v-row class="content-container">
-    <v-col lg="12" class="exmp">
-      About
+    <v-col
+      lg="12"
+      class="exmp"
+      data-aos="fade-down"
+      data-aos-dealy="400"
+      data-aos-duration="1200"
+      data-aos-once="true"
+    >
+      {{ aboutText }}
     </v-col>
     <v-row style="margin-top: -200px;">
-      <v-col lg="6" class="image-section">
+      <v-col
+        lg="6"
+        class="image-section"
+        data-aos="fade-right"
+        data-aos-dealy="800"
+        data-aos-duration="1200"
+        data-aos-once="true"
+      >
         <img :src="profileImage" alt="" class="profile-image" />
       </v-col>
-      <v-col lg="6">
+      <v-col
+        lg="6"
+        data-aos="fade-up"
+        data-aos-dealy="1200"
+        data-aos-duration="1200"
+        data-aos-once="true"
+      >
         <div>
           <div class="full-name">
-            Jakub Gania
+            {{ fullname }}
           </div>
           <div class="xmd">
-            <span>ABOUT</span>
+            <span>
+              {{ aboutText }}
+            </span>
           </div>
           <div style="padding-left: 50px;">
             <div class="description">
               {{ description }}
             </div>
             <div class="icons-section">
-              <v-icon>mdi-github-circle</v-icon>
-              <v-icon>mdi-linkedin</v-icon>
+              <a :href="link.github" target="_blank" class="icon-link">
+                <v-icon class="icon">mdi-github-circle</v-icon>
+              </a>
+              <a :href="link.linkedin" target="_blank" class="icon-link">
+                <v-icon class="icon">mdi-linkedin</v-icon>
+              </a>
             </div>
             <div class="contact-button-section">
               contact button
@@ -40,8 +66,14 @@ export default {
   data() {
     return {
       profileImage,
+      aboutText: 'About',
+      fullname: 'Jakub Gania',
       description:
-        'Nazywam się Jakub Gania. Mieszkam we Wrocławiu. W 2018 roku uzyskałem tytuł inżyniera informatki z specjalizacją inżyniera systemów informatycznych. Specjalizuję sie w technologiach webowych. Cały czas się uczę i poszerzam swoją wiedzę w różnych obszarach powiązanych z programowaniem. Moje projekty udostępniam na platformie GitHub. Rozwijam się w kierunku Full Stack Web Developer.'
+        'Nazywam się Jakub Gania. Mieszkam we Wrocławiu. W 2018 roku uzyskałem tytuł inżyniera informatki z specjalizacją inżyniera systemów informatycznych. Specjalizuję sie w technologiach webowych. Cały czas się uczę i poszerzam swoją wiedzę w różnych obszarach powiązanych z programowaniem. Moje projekty udostępniam na platformie GitHub. Rozwijam się w kierunku Full Stack Web Developer.',
+      link: {
+        github: 'https://github.com/jakubgania',
+        linkedin: 'https://pl.linkedin.com/in/jakubgania'
+      }
     }
   }
 }
@@ -88,11 +120,12 @@ export default {
   line-height: 0.01em;
   margin: 10px 0 20px;
   position: absolute;
-  top: 442px;
+  top: 200px;
 }
 .xmd span {
   background-color: #fafafa;
   padding: 0 10px;
+  text-transform: uppercase;
 }
 .full-name {
   font-size: 60px;
@@ -108,5 +141,11 @@ export default {
 }
 .contact-button-section {
   margin-top: 20px;
+}
+.icon-link {
+  text-decoration: none;
+}
+.icon:hover {
+  color: black;
 }
 </style>
