cdccf64808e1761aef1999f7a6265fd9a07ae9d8
jakubgania <jakubgania@outlook.com>
2019-09-21 01:13:34 +0200

    implemented toolbar-header component

---
+++ components/toolbar-header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-if="displayHeader()">
+    <v-app-bar absolute app clipped-left flat>
+      <v-toolbar-title>
+        <nuxt-link to="/" class="link-title">
+          <div>
+            Jakub Gania Software
+          </div>
+        </nuxt-link>
+      </v-toolbar-title>
+      <v-spacer />
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    displayHeader() {
+      return this.$route.name !== 'index'
+    }
+  },
+  mounted() {
+    this.displayHeader()
+  }
+}
+</script>
+
+<style scoped>
+.link-title {
+  text-decoration: none;
+  font-family: 'Audiowide', cursive;
+  color: black;
+  font-size: 24px;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,8 @@
 <template>
   <v-app>
-    <v-content>
+    <v-content style="padding-top: 0;">
+      <toolbar-header-component />
+
       <v-container fluid style="padding: 0;">
         <nuxt />
       </v-container>
@@ -9,7 +11,12 @@
 </template>
 
 <script>
+import ToolbarHeaderComponent from '../components/toolbar-header'
+
 export default {
+  components: {
+    'toolbar-header-component': ToolbarHeaderComponent
+  },
   data() {
     return {
       //
