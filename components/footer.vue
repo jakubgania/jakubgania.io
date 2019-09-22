8528b26a36786ba307a945a1da4e54e8df8e1a8e
jakubgania <jakubgania@outlook.com>
2019-09-22 12:55:21 +0200

    implemented footer component

---
+++ components/footer.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-row v-if="displayFooter()" class="footer-container">
+    <v-col lg="6">
+      <a :href="links.github.url" target="_blank" class="link">
+        {{ links.github.title }}
+      </a>
+      <a :href="links.linkedin.url" target="_blank" class="link">
+        {{ links.linkedin.title }}
+      </a>
+    </v-col>
+    <v-col lg="6" style="text-align: right;">
+      <div v-html="copyrightText" />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: {
+        github: {
+          title: 'Github',
+          url: 'https://github.com/jakubgania'
+        },
+        linkedin: {
+          title: 'Linkedin',
+          url: 'https://www.linkedin.com/in/jakubgania'
+        }
+      },
+      copyrightText: 'Jakub Gania Software &copy; 2018 - 2019'
+    }
+  },
+  methods: {
+    displayFooter() {
+      return this.$route.name !== 'index'
+    }
+  },
+  mounted() {
+    this.displayFooter()
+  }
+}
+</script>
+
+<style scoped>
+.footer-container {
+  max-width: 1400px;
+  width: 100%;
+  font-size: 12px;
+  letter-spacing: 1px;
+  border-top: 1px solid #000;
+  margin: auto;
+  height: 80px;
+  line-height: 80px;
+  margin-bottom: 20px;
+}
+.link {
+  text-decoration: none;
+  color: black;
+}
+.link:hover {
+  color: blue;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-content style="padding-top: 0;">
+    <v-content class="app-content-container" style="padding-top: 0;">
       <navigation-drawer-component
         :drawer="drawer"
         @switchNavigationDrawer="switchNavigationDrawer"
@@ -15,17 +15,21 @@
         <nuxt />
       </v-container>
     </v-content>
+
+    <footer-component />
   </v-app>
 </template>
 
 <script>
 import ToolbarHeaderComponent from '../components/toolbar-header'
 import NavigationDrawerComponent from '../components/navigation-drawer'
+import FooterComponent from '../components/footer'
 
 export default {
   components: {
     'toolbar-header-component': ToolbarHeaderComponent,
-    'navigation-drawer-component': NavigationDrawerComponent
+    'navigation-drawer-component': NavigationDrawerComponent,
+    'footer-component': FooterComponent
   },
   data() {
     return {
@@ -42,3 +46,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.app-content-container {
+  min-height: 100vh;
+}
+</style>
