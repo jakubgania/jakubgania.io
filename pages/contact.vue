7a135d508c1ddfd57e16d9666037f7c146a50a18
jakubgania <jakubgania@outlook.com>
2019-09-22 22:29:53 +0200

    added contact subpage

---
+++ pages/contact.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-row class="content-container">
+    <v-col lg="12" style="padding: 0;">
+      <div class="page-title">
+        Kontakt
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<style scoped>
+.content-container {
+  max-width: 800px;
+  width: 100%;
+  margin: auto;
+}
+.page-title {
+  font-size: 40px;
+  border-bottom: 2px solid black;
+  margin-top: 80px;
+  font-weight: 800;
+}
+</style>
