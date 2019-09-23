89c4fe9cbab8b1482f53fd466522938b08fd5648
jakubgania <jakubgania@outlook.com>
2019-09-23 23:48:45 +0200

    added subpage with params for shortcut subpage

---
+++ pages/shortcuts/_id.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-row class="content-container">
+    <v-col lg="12" style="padding: 0;">
+      <div class="page-title">
+        {{ pageTitle + ' ' + id }}
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      pageTitle: 'Skrt'
+    }
+  },
+  asyncData({ params }) {
+    return { id: params.id }
+  }
+}
+</script>
+
+<style scoped>
+.content-container {
+  max-width: 800px;
+  width: 100%;
+  margin: auto;
+}
+.page-title {
+  font-size: 40px;
+  border-bottom: 2px solid black;
+  margin-top: 80px;
+  font-weight: 800;
+}
+</style>
