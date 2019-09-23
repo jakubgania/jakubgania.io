018e0812fdb8d063a913ce2070342e6f1114dbc4
jakubgania <jakubgania@outlook.com>
2019-09-23 22:43:53 +0200

    updated navigation-drawer component

--- components/navigation-drawer.vue
+++ components/navigation-drawer.vue
@@ -14,27 +14,37 @@
         class="close-icon-button"
         @click.native="$emit('switchNavigationDrawer')"
       >
-        <v-icon>mdi-close</v-icon>
+        <v-icon>
+          mdi-close
+        </v-icon>
       </v-btn>
     </div>
     <v-list dense nav>
       <template v-for="item in items">
         <v-list-item v-if="item.path" :key="item.title" :to="item.path">
           <v-list-item-icon>
-            <v-icon>mdi-chevron-right</v-icon>
+            <v-icon class="list-item-icon">
+              {{ menuIcon }}
+            </v-icon>
           </v-list-item-icon>
 
           <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
+            <v-list-item-title class="list-item-title">
+              {{ item.title }}
+            </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
         <v-list-item v-if="item.url" :key="item.title" :href="item.url">
           <v-list-item-icon>
-            <v-icon>mdi-chevron-right</v-icon>
+            <v-icon class="list-item-icon">
+              {{ menuIcon }}
+            </v-icon>
           </v-list-item-icon>
 
           <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
+            <v-list-item-title class="list-item-title">
+              {{ item.title }}
+            </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </template>
@@ -54,7 +64,8 @@ export default {
   },
   data() {
     return {
-      items: menuItems.menu
+      items: menuItems.menu,
+      menuIcon: 'mdi-chevron-right'
     }
   },
   computed: {
@@ -75,9 +86,15 @@ export default {
   text-align: right;
   height: 64px;
   line-height: 64px;
-  background-color: aliceblue;
 }
 .close-icon-button {
   margin-right: 10px;
+}
+.list-item-title {
+  letter-spacing: 2px;
+  color: #8c8c8c;
+}
+.list-item-icon {
+  color: #8c8c8c;
 }
 </style>
