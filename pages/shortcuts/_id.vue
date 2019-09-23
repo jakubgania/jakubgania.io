d3c096122e0178df53e8a80b09392f4050afda58
jakubgania <jakubgania@outlook.com>
2019-09-24 00:31:26 +0200

    implementation subpage with params for shortcut subpage

--- pages/shortcuts/_id.vue
+++ pages/shortcuts/_id.vue
@@ -1,22 +1,55 @@
 <template>
   <v-row class="content-container">
-    <v-col lg="12" style="padding: 0;">
-      <div class="page-title">
-        {{ pageTitle + ' ' + id }}
-      </div>
-    </v-col>
+    <template v-if="!error">
+      <v-col lg="12" style="padding: 0;">
+        <div class="page-title">
+          {{ pageTitle + ' ' + id }}
+        </div>
+      </v-col>
+      <v-col v-for="item in data" :key="item.id" lg="12">
+        <div>
+          {{ item.shortcut }}
+        </div>
+      </v-col>
+    </template>
+    <template v-else>
+      <div style="margin-top: 80px;">fetch error</div>
+    </template>
   </v-row>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data() {
     return {
-      pageTitle: 'Skrt'
+      pageTitle: 'Skrt',
+      error: false
     }
   },
-  asyncData({ params }) {
-    return { id: params.id }
+  asyncData({ params, error }) {
+    // import shortcuts from `../json/shortcuts/${params.id}.json`
+
+    // const { data } = await axios.get(
+    //   'http://localhost:3000/json/shortcuts/chrome.json'
+    // )
+
+    return axios
+      .get(`http://127.0.0.1:3000/shortcuts/${params.id}.json`)
+      .then((res) => {
+        return {
+          id: params.id,
+          data: res.data.items
+        }
+      })
+      .catch(() => {
+        return {
+          error: true
+        }
+      })
+
+    // return { shortcuts: data }
   }
 }
 </script>
