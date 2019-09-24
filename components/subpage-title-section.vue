de448d1f1cd867bf3f6f1332bf1dc68eabfc1def
jakubgania <jakubgania@outlook.com>
2019-09-25 00:12:30 +0200

    implementation subpage-title-section component

---
+++ components/subpage-title-section.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-col lg="12" style="padding: 0;">
+    <div class="page-title">
+      {{ title }}
+    </div>
+  </v-col>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.page-title {
+  font-size: 40px;
+  margin-top: 80px;
+  font-weight: 800;
+  border-bottom: 2px solid #000;
+}
+</style>
