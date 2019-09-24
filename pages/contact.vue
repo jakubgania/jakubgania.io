83f07fb07a6a9267c5c7e501ac1a161d491c3226
jakubgania <jakubgania@outlook.com>
2019-09-24 18:27:55 +0200

    updated contact subpage

--- pages/contact.vue
+++ pages/contact.vue
@@ -5,6 +5,11 @@
         Kontakt
       </div>
     </v-col>
+    <v-col lg="12">
+      <div class="email">
+        contact@jakubgania.io
+      </div>
+    </v-col>
   </v-row>
 </template>
 
@@ -19,5 +24,13 @@
   border-bottom: 2px solid black;
   margin-top: 80px;
   font-weight: 800;
+  /* letter-spacing: 2px; */
+}
+.email {
+  padding-top: 80px;
+  text-align: center;
+  color: black;
+  letter-spacing: 2px;
+  font-size: 12px;
 }
 </style>
