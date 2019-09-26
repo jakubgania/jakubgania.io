e9c7161d0605d0b4b4802c5d6cfc680f520b2c4f
jakubgania <jakubgania@outlook.com>
2019-09-26 21:00:42 +0200

    added title component for posts/_id subpage

--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -1,11 +1,7 @@
 <template>
   <v-row class="content-container">
     <template v-if="!error">
-      <v-col lg="12" style="padding: 0;">
-        <div class="page-title">
-          {{ pageTitle }}
-        </div>
-      </v-col>
+      <subpage-title-section-component :title="title" />
     </template>
     <template v-else>
       <div style="margin-top: 80px;">fetch error</div>
@@ -14,12 +10,15 @@
 </template>
 
 <script>
-// import axios from 'axios'
+import SubpageTitleSection from '../../components/subpage-title-section'
 
 export default {
+  components: {
+    'subpage-title-section-component': SubpageTitleSection
+  },
   data() {
     return {
-      pageTitle: 'Example post title',
+      title: 'Example post title  ',
       error: false
     }
   },
@@ -50,11 +49,5 @@ export default {
   max-width: 800px;
   width: 100%;
   margin: auto;
-}
-.page-title {
-  font-size: 40px;
-  border-bottom: 2px solid black;
-  margin-top: 80px;
-  font-weight: 800;
 }
 </style>
