c27f27ae1794e1d948b9851084513a2510677f2f
jakubgania <jakubgania@outlook.com>
2019-10-03 22:17:57 +0200

    implementation of the new design on menu subpage

--- pages/menu.vue
+++ pages/menu.vue
@@ -5,26 +5,80 @@
     <v-row class="content-container" no-gutters>
       <subpage-title-section-component :title="title" />
 
+      <v-col cols="12" style="margin-top: 20px;letter-spacing: 2px;">
+        <span style="color: #4d4dff;font-weight: 600;">data</span>
+        <span style="color: #cc33ff;font-weight: 600;">()</span>
+        <span style="color: #cc33ff;font-weight: 600;">{</span>
+      </v-col>
+      <v-col cols="12" style="letter-spacing: 2px;">
+        <span style="color: #6699ff;font-weight: 600;">&nbsp;&nbsp;return</span>
+        <span style="color: #6699ff;font-weight: 600;">{</span>
+      </v-col>
+      <v-col cols="12">
+        <div style="letter-spacing: 2px;font-weight: 600;">
+          &nbsp;&nbsp;&nbsp;&nbsp;items: <span style="color: orange;">[</span>
+        </div>
+      </v-col>
+
       <v-col
-        v-for="item in items"
+        v-for="(item, index) in items"
         :key="item.id"
         cols="12"
         class="link-section"
+        style="font-weight: 600;"
       >
         <template v-if="item.path">
           <nuxt-link :to="item.path" class="link">
+            <div class="link-x">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <span style="color: #cc33ff;">{</span>
+            </div>
             <div class="link-text">
-              {{ item.title }}
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <span style="color: #8c8c8c;">page:</span>
+              {{ "'" + item.title + "'" }}
+            </div>
+            <div class="link-x">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <span style="color: #cc33ff;">
+                } <span class="comma">,</span>
+              </span>
             </div>
           </nuxt-link>
         </template>
         <template v-if="item.url">
           <a :href="item.url" target="_blank" class="link">
+            <div class="link-x">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <span style="color: #cc33ff;">{</span>
+            </div>
             <div class="link-text">
-              {{ item.title }}
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <span style="color: #8c8c8c;">page:</span>
+              {{ "'" + item.title + "'" }}
+            </div>
+            <div class="link-x">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <span style="color: #cc33ff;">
+                }
+                <span class="comma" :class="{ 'comma-not': index == 10 }">
+                  ,
+                </span>
+              </span>
             </div>
           </a>
         </template>
+      </v-col>
+
+      <v-col cols="12">
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <span style="color: orange;font-weight: 600;">]</span>
+      </v-col>
+      <v-col cols="12">
+        <span style="color: #6699ff;font-weight: 600;">&nbsp;&nbsp;}</span>
+      </v-col>
+      <v-col cols="12">
+        <span style="color: #cc33ff;font-weight: 600;">}</span>
       </v-col>
     </v-row>
   </v-row>
@@ -63,19 +117,13 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .content-container {
   max-width: 800px;
   width: 100%;
-  height: 600px;
   margin: auto;
   margin-bottom: 100px;
-}
-.page-title {
-  font-size: 40px;
-  border-bottom: 2px solid black;
-  margin-top: 80px;
-  font-weight: 800;
+  font-size: 12px;
 }
 .link {
   text-decoration: none;
@@ -84,14 +132,29 @@ export default {
   color: black;
   letter-spacing: 2px;
   font-size: 12px;
-  padding-top: 12px;
-  padding-bottom: 12px;
+  padding-top: 2px;
+  padding-bottom: 2px;
+}
+.link-x {
+  padding: 0;
 }
 .link-section:hover {
   background-color: #e6e6e6;
-  letter-spacing: 4px;
+  // letter-spacing: 4px;
   padding-left: 12px;
   transition: padding-left 0.2s ease;
+
+  // &.link-x {
+  //   letter-spacing: 4px;
+  //   padding-left: 12px;
+  //   transition: padding-left 0.2s ease;
+  // }
+}
+.comma {
+  color: #6699ff;
+}
+.comma-not {
+  display: none;
 }
 .top-image {
   width: 100%;
