4b8c787a305fdc6ba97d1e370d0e9b1ea6e71441
jakubgania <jakubgania@outlook.com>
2019-10-04 00:01:55 +0200

    implemented new list on navigation-drawer component

--- components/navigation-drawer.vue
+++ components/navigation-drawer.vue
@@ -5,7 +5,7 @@
     app
     right
     temporary
-    width="300"
+    width="320"
   >
     <div class="close-icon-section">
       <v-btn
@@ -20,34 +20,64 @@
       </v-btn>
     </div>
     <v-list dense nav>
+      <v-list-item style="margin-bottom: 0;">
+        <v-list-item-content style="padding-bottom: 0;">
+          <v-list-item-title class="non-list-element">
+            <span style="color: #ff0066;">switch</span>
+            ($menu) {
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
       <template v-for="item in items">
         <v-list-item v-if="item.path" :key="item.title" :to="item.path">
-          <v-list-item-icon>
-            <v-icon class="list-item-icon">
-              {{ menuIcon }}
-            </v-icon>
-          </v-list-item-icon>
-
           <v-list-item-content>
             <v-list-item-title class="list-item-title">
-              {{ item.title }}
+              <span style="display: block;">
+                &nbsp;&nbsp;
+                <span style="color: #ff0066;">case</span>
+                {{ "'" + item.path + "'" }}:
+              </span>
+              <span class="xdp">
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <span style="color: #005ce6;">echo:</span>
+                <span style="color: black;">{{ "'" + item.title + "'" }}</span>
+              </span>
+              <span style="display: block;">
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <span style="color: #ff0066;">break;</span>
+              </span>
             </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
         <v-list-item v-if="item.url" :key="item.title" :href="item.url">
-          <v-list-item-icon>
-            <v-icon class="list-item-icon">
-              {{ menuIcon }}
-            </v-icon>
-          </v-list-item-icon>
-
           <v-list-item-content>
             <v-list-item-title class="list-item-title">
-              {{ item.title }}
+              <span style="display: block;">
+                &nbsp;&nbsp;
+                <span style="color: #ff0066;">case</span>
+                {{ "'" + item.name + "'" }}:
+              </span>
+              <span class="echo-item">
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <span style="color: #005ce6;">echo:</span>
+                <span style="color: black;">{{ "'" + item.title + "'" }}</span>
+              </span>
+              <span style="display: block;">
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <span style="color: #ff0066;">break;</span>
+              </span>
             </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </template>
+
+      <v-list-item style="margin-top: 0;">
+        <v-list-item-content style="padding-top: 0;">
+          <v-list-item-title class="non-list-element">
+            }
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
     </v-list>
   </v-navigation-drawer>
 </template>
@@ -95,6 +125,16 @@ export default {
   color: #8c8c8c;
 }
 .list-item-icon {
+  color: #8c8c8c;
+}
+.echo-item {
+  display: block;
+  color: black;
+  padding-top: 4px;
+  padding-bottom: 4px;
+}
+.non-list-element {
+  letter-spacing: 2px;
   color: #8c8c8c;
 }
 </style>
