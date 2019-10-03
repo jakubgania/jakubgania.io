59a48ac9c6c06d6fb7d70368f5506a7bdfef29f9
jakubgania <jakubgania@outlook.com>
2019-10-03 11:47:12 +0200

    implemented subpage-description component

---
+++ components/subpage-description.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-col cols="12" lg="12" xs="12" style="padding: 0;">
+    <div class="page-description">
+      {{ description }}
+    </div>
+  </v-col>
+</template>
+
+<script>
+export default {
+  props: {
+    description: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.page-description {
+  font-size: 20px;
+  margin-top: 10px;
+  font-weight: 800;
+  border-bottom: 2px solid #000;
+}
+</style>
