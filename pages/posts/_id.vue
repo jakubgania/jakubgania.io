3ee84af39f0888c6866362a9011cfbac26425ad4
jakubgania <jakubgania@outlook.com>
2019-10-08 20:36:49 +0200

    implemented posts-sidebar-menu component

--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -11,25 +11,7 @@
         <div class="post" v-html="$md.render(model)"></div>
       </div>
 
-      <div class="sidebar">
-        <div class="sidebar-menu">
-          <div class="sidebar-title">
-            ostatnie posty
-          </div>
-          <div class="sidebar-section">
-            brak
-          </div>
-          <div class="sidebar-section">
-            <div v-for="item in social" :key="item.id">
-              <a :href="item.url" class="social-link" target="_blank">
-                <div class="social-link-text">
-                  {{ item.title }}
-                </div>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+      <posts-sidebar-menu-component />
     </template>
     <template v-else>
       <v-col lg="12">
@@ -50,10 +32,12 @@
 import axios from 'axios'
 import frontmatter from 'front-matter'
 import SubpageTitleSection from '../../components/subpage-title-section'
+import PostsSidebarMenu from '../../components/posts-sidebar-menu'
 
 export default {
   components: {
-    'subpage-title-section-component': SubpageTitleSection
+    'subpage-title-section-component': SubpageTitleSection,
+    'posts-sidebar-menu-component': PostsSidebarMenu
   },
   data() {
     return {
@@ -61,17 +45,7 @@ export default {
       error: false,
       ht: 'Jakub Gania Software - Post - ',
       errorMessage: 'Wystąpił błąd pobierania lub taki post nie istnieje',
-      marginTop: 0,
-      social: [
-        {
-          title: 'github',
-          url: 'https://github.com/jakubgania'
-        },
-        {
-          title: 'linkedin',
-          url: 'https://linkedin.com/in/jakubgania/'
-        }
-      ]
+      marginTop: 0
     }
   },
   async asyncData({ params }) {
@@ -146,46 +120,5 @@ export default {
   line-height: 1.8;
   font-size: 16.8px;
   padding-top: 20px;
-}
-.sidebar {
-  max-width: 400px;
-  width: 100%;
-  font-family: 'Nunito', sans-serif;
-}
-div.sidebar-menu {
-  top: 60px;
-  position: sticky;
-  height: auto;
-  margin-left: 40px;
-  margin-top: 42px;
-}
-.social-link {
-  text-decoration: none;
-}
-.social-link-text {
-  width: 100%;
-  letter-spacing: 1px;
-  color: black;
-  padding-top: 4px;
-  padding-bottom: 4px;
-}
-.social-link-text:hover {
-  color: #0066ff;
-  padding-left: 12px;
-  background-color: #e6e6e6;
-  transition: padding-left 0.2s ease;
-}
-.sidebar-title {
-  border-top: 2px solid black;
-  padding-top: 20px;
-  padding-bottom: 10px;
-  text-transform: uppercase;
-  letter-spacing: 2px;
-  font-size: 14px;
-  font-weight: 700;
-}
-.sidebar-section {
-  padding-top: 10px;
-  padding-bottom: 20px;
 }
 </style>
