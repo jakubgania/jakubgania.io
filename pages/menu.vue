c00086384df5c008b8a512fc3619c9de7ac550b9
jakubgania <jakubgania@outlook.com>
2019-10-15 09:25:33 +0200

    improved html code formatting

--- pages/menu.vue
+++ pages/menu.vue
@@ -6,13 +6,23 @@
       <subpage-title-section-component :title="title" />
 
       <v-col cols="12" style="margin-top: 20px;letter-spacing: 2px;">
-        <span style="color: #4d4dff;font-weight: 600;">data</span>
-        <span style="color: #cc33ff;font-weight: 600;">()</span>
-        <span style="color: #cc33ff;font-weight: 600;">{</span>
+        <span style="color: #4d4dff;font-weight: 600;">
+          data
+        </span>
+        <span style="color: #cc33ff;font-weight: 600;">
+          ()
+        </span>
+        <span style="color: #cc33ff;font-weight: 600;">
+          {
+        </span>
       </v-col>
       <v-col cols="12" style="letter-spacing: 2px;">
-        <span style="color: #6699ff;font-weight: 600;">&nbsp;&nbsp;return</span>
-        <span style="color: #6699ff;font-weight: 600;">{</span>
+        <span style="color: #6699ff;font-weight: 600;">
+          &nbsp;&nbsp;return
+        </span>
+        <span style="color: #6699ff;font-weight: 600;">
+          {
+        </span>
       </v-col>
       <v-col cols="12">
         <div style="letter-spacing: 2px;font-weight: 600;">
@@ -35,7 +45,9 @@
             </div>
             <div class="link-text">
               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <span style="color: #8c8c8c;">page:</span>
+              <span style="color: #8c8c8c;">
+                page:
+              </span>
               {{ "'" + item.title + "'" }}
             </div>
             <div class="link-x">
@@ -50,11 +62,15 @@
           <a :href="item.url" target="_blank" class="link">
             <div class="link-x">
               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <span style="color: #cc33ff;">{</span>
+              <span style="color: #cc33ff;">
+                {
+              </span>
             </div>
             <div class="link-text">
               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <span style="color: #8c8c8c;">page:</span>
+              <span style="color: #8c8c8c;">
+                page:
+              </span>
               {{ "'" + item.title + "'" }}
             </div>
             <div class="link-x">
@@ -72,13 +88,19 @@
 
       <v-col cols="12">
         &nbsp;&nbsp;&nbsp;&nbsp;
-        <span style="color: orange;font-weight: 600;">]</span>
+        <span style="color: orange;font-weight: 600;">
+          ]
+        </span>
       </v-col>
       <v-col cols="12">
-        <span style="color: #6699ff;font-weight: 600;">&nbsp;&nbsp;}</span>
+        <span style="color: #6699ff;font-weight: 600;">
+          &nbsp;&nbsp;}
+        </span>
       </v-col>
       <v-col cols="12">
-        <span style="color: #cc33ff;font-weight: 600;">}</span>
+        <span style="color: #cc33ff;font-weight: 600;">
+          }
+        </span>
       </v-col>
     </v-row>
   </v-row>
