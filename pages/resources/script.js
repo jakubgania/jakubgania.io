b281466f6b00041025328b95267cc64bec5c8708
jakubgania <jakubgania@outlook.com>
2019-10-29 22:20:55 +0100

    changing resources subpage component to separate components: index.vue, script.js, style.scss

---
+++ pages/resources/script.js
@@ -0,0 +1,45 @@
+import SubpageTitleSection from '../../components/subpage-title-section'
+import SubpageDescriptionSection from '../../components/subpage-description-section'
+import linksData from '../../json/resources-items.json'
+
+export default {
+  components: {
+    'subpage-title-section-component': SubpageTitleSection,
+    'subpage-description-section-component': SubpageDescriptionSection
+  },
+  data() {
+    return {
+      title: '{ zasoby }',
+      description:
+        'Na tej stronie zebrane są różne linki do takich stron jak portale technologiczne, blogi, kanały na Youtube, dokumentacje projektów technologicznych. Obecnie materiały są podzielone z grubsza na kategorie ale strona będzie pod tym względem na pewno ciągle modyfikowana. Cały czas znaujduję różne ciekawe artykłu i  ta lista będzie aktualizowana żeby zebrać to wszystko centralnie w jednym miejscu.',
+      linksData: linksData.resources_items
+    }
+  },
+  computed: {
+    titleMarginTop() {
+      switch (this.$vuetify.breakpoint.name) {
+        case 'xs':
+          return { marginTop: 30, fontSize: 22 }
+        default:
+          return { marginTop: 120, fontSize: 28 }
+      }
+    }
+  },
+  head() {
+    return {
+      title: 'Jakub Gania Software - Zasoby',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Zasoby'
+        },
+        {
+          name: 'keywords',
+          content:
+            'programy, strony, portale, portale technologiczne, portale programistyczne, dokumentacje, projekty, blogi, artykuły, kanały youtube'
+        }
+      ]
+    }
+  }
+}
