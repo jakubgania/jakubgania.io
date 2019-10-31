dc26d41533fbdb2817622ae3783b8f6f0ced7957
jakubgania <jakubgania@outlook.com>
2019-10-31 19:04:46 +0100

    updated projects subpage on mobile breakpoint

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,6 +1,10 @@
 <template>
   <v-row class="content-container">
-    <subpage-title-section-component :title="title" />
+    <subpage-title-section-component
+      :title="title"
+      :margin-top="titleMarginTop.marginTop"
+      :font-size="titleMarginTop.fontSize"
+    />
 
     <subpage-description-section-component :description="description" />
 
@@ -27,176 +31,11 @@
 </template>
 
 <script>
-import SubpageTitleSection from '../../components/subpage-title-section'
-import SubpageDescriptionSection from '../../components/subpage-description-section'
+import script from './script.js'
 
-export default {
-  components: {
-    'subpage-title-section-component': SubpageTitleSection,
-    'subpage-description-section-component': SubpageDescriptionSection
-  },
-  data() {
-    return {
-      title: '{ projekty }',
-      description:
-        'Lista moich projektów z odnośnikami do szczegółowych opisów. Obecnie strona w przygotowaniu.',
-      projects: [
-        {
-          title: 'jakubgania.io',
-          path: 'jakubgania.io'
-        },
-        {
-          title: 'bash-scripts',
-          path: 'bash-scripts'
-        },
-        {
-          title: 'jakubgania.io-api',
-          path: 'jakubgania.io-api'
-        },
-        {
-          title: 'yourcity-frontend',
-          path: 'yourcity-frontend'
-        },
-        {
-          title: 'javascript-scripts',
-          path: 'javascript-scripts'
-        },
-        {
-          title: 'dockermanager-frontend',
-          path: 'dockermanager-frontend'
-        },
-        {
-          title: 'yourcity-backend-laravel',
-          path: 'yourcity-backend-laravel'
-        },
-        {
-          title: 'dockermanager-backend',
-          path: 'dockermanager-backend'
-        },
-        {
-          title: 'yourcity-app-frontend',
-          path: 'yourcity-app-frontend'
-        },
-        {
-          title: 'php-scripts',
-          path: 'php-scripts'
-        },
-        {
-          title: 'python-scripts',
-          path: 'python-scripts'
-        },
-        {
-          title: 'yourcity-app-backend',
-          path: 'yourcity-app-backend'
-        },
-        {
-          title: 'jakubgania-app-environment-setup',
-          path: 'jakubgania-app-environment-setup'
-        },
-        {
-          title: 'cutdown-timer-page',
-          path: 'cutdown-timer-page'
-        },
-        {
-          title: 'yourcity-app-environment-setup',
-          path: 'yourcity-app-environment-setup'
-        },
-        {
-          title: 'jakubgania.io-old-version',
-          path: 'jakubgania.io-old-version'
-        },
-        {
-          title: 'vue-infinite-scroll',
-          path: 'vue-infinite-scroll'
-        },
-        {
-          title: 'coding-conventions',
-          path: 'coding-conventions'
-        },
-        {
-          title: 'angular-6-examples-app',
-          path: 'angular-6-examples-app'
-        },
-        {
-          title: 'typescript-examples-from-tutorial',
-          path: 'typescript-examples-from-tutorial'
-        },
-        {
-          title: 'powershell-scripts',
-          path: 'powershell-scripts'
-        },
-        {
-          title: 'toggle-features-platform',
-          path: 'toggle-features-platform'
-        },
-        {
-          title: 'yourcity-admin-panel',
-          path: 'yourcity-admin-panel'
-        },
-        {
-          title: 'yourcity-backend-express',
-          path: 'yourcity-backend-express'
-        },
-        {
-          title: 'projects-of-arduino-for-college',
-          path: 'projects-of-arduino-for-college'
-        },
-        {
-          title: 'prototype-social-portal-backend',
-          path: 'prototype-social-portal-backend'
-        },
-        {
-          title: 'prototype-social-portal-frontend',
-          path: 'prototype-social-portal-backend'
-        },
-        {
-          title: 'retnal-property',
-          path: 'retnal-property'
-        }
-      ]
-    }
-  },
-  head() {
-    return {
-      title: 'Jakub Gania Software - Projekty',
-      meta: [
-        {
-          hid: 'description',
-          name: 'description',
-          content: 'Projekty'
-        },
-        {
-          name: 'keywords',
-          content: 'Projekty'
-        }
-      ]
-    }
-  }
-}
+export default script
 </script>
 
-<style scoped>
-.content-container {
-  max-width: 800px;
-  width: 100%;
-  margin: auto;
-  margin-bottom: 80px;
-}
-.sctn {
-  padding: 0;
-  padding-top: 10px;
-  padding-bottom: 10px;
-}
-.sctn:hover {
-  background-color: #e6e6e6;
-  padding-left: 12px;
-  transition: padding-left 0.2s ease;
-  color: blue;
-}
-.link {
-  text-decoration: none;
-  letter-spacing: 1px;
-  font-size: 14px;
-  color: black;
-}
+<style lang="scss" scoped>
+@import 'style.scss';
 </style>
