7fc3d84e1ab4e35933fc702d69ec78275b50167e
jakubgania <jakubgania@outlook.com>
2019-11-02 16:31:33 +0100

    updated menu component

--- pages/menu/index.vue
+++ pages/menu/index.vue
@@ -11,7 +11,7 @@
 
       <v-col cols="12" style="margin-top: 20px;letter-spacing: 2px;">
         <span style="color: #4d4dff;font-weight: 600;">
-          &nbsp;<span class="number-line">1</span> data
+          &nbsp;<span class="number-line">1</span>&nbsp;&nbsp;data
         </span>
         <span style="color: #cc33ff;font-weight: 600;">
           ()
@@ -22,7 +22,7 @@
       </v-col>
       <v-col cols="12" style="letter-spacing: 2px;">
         <span style="color: #6699ff;font-weight: 600;">
-          &nbsp;<span class="number-line">2</span> &nbsp;&nbsp;return
+          &nbsp;<span class="number-line">2</span>&nbsp;&nbsp;&nbsp;&nbsp;return
         </span>
         <span style="color: #6699ff;font-weight: 600;">
           {
@@ -31,7 +31,8 @@
       <v-col cols="12">
         <div style="letter-spacing: 2px;font-weight: 600;">
           &nbsp;<span class="number-line">3</span>
-          &nbsp;&nbsp;&nbsp;&nbsp;items: <span style="color: orange;">[</span>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;items:
+          <span style="color: orange;">[</span>
         </div>
       </v-col>
 
@@ -49,27 +50,29 @@
                 &nbsp;
               </template>
               <span class="number-line">{{ index * 3 + 1 + 3 }}</span>
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <span style="color: #cc33ff;">{</span>
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <span class="xpo" style="color: #cc33ff;">{</span>
             </div>
             <div class="">
               <template v-if="index * 3 + 2 + 3 < 10">
                 &nbsp;
               </template>
               <span class="number-line">{{ index * 3 + 2 + 3 }}</span>
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <span class="link-text" style="color: #8c8c8c;">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <span class="link-text xpo" style="color: #8c8c8c;">
                 page:
               </span>
-              <span class="link-text">{{ "'" + item.title + "'" }}</span>
+              <span class="link-text">
+                {{ "'" + item.title + "'" }}
+              </span>
             </div>
             <div class="link-x">
               <template v-if="index * 3 + 3 + 3 < 10">
                 &nbsp;
               </template>
               <span class="number-line">{{ index * 3 + 3 + 3 }}</span>
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <span style="color: #cc33ff;">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <span class="xpo" style="color: #cc33ff;">
                 } <span class="comma">,</span>
               </span>
             </div>
@@ -79,23 +82,23 @@
           <a :href="item.url" target="_blank" class="link">
             <div class="link-x">
               <span class="number-line">{{ index * 3 + 1 + 3 }}</span>
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <span style="color: #cc33ff;">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <span class="xpo" style="color: #cc33ff;">
                 {
               </span>
             </div>
             <div class="">
               <span class="number-line">{{ index * 3 + 2 + 3 }}</span>
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <span class="link-text" style="color: #8c8c8c;">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <span class="link-text xpo" style="color: #8c8c8c;">
                 page:
               </span>
               <span class="link-text">{{ "'" + item.title + "'" }}</span>
             </div>
             <div class="link-x">
               <span class="number-line">{{ index * 3 + 3 + 3 }}</span>
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <span style="color: #cc33ff;">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <span class="xpo" style="color: #cc33ff;">
                 }
                 <span
                   class="comma"
@@ -110,22 +113,28 @@
       </v-col>
 
       <v-col cols="12">
-        <span class="number-line" style="font-weight: 600;">40</span>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <span class="number-line" style="font-weight: 600;">
+          40
+        </span>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         <span style="color: orange;font-weight: 600;">
           ]
         </span>
       </v-col>
       <v-col cols="12">
-        <span class="number-line" style="font-weight: 600;">41</span>
+        <span class="number-line" style="font-weight: 600;">
+          41
+        </span>
         &nbsp;&nbsp;
         <span style="color: #6699ff;font-weight: 600;">
-          &nbsp;&nbsp;}
+          &nbsp;&nbsp;&nbsp;}
         </span>
       </v-col>
       <v-col cols="12">
-        <span class="number-line" style="font-weight: 600;">42</span>
-        &nbsp;&nbsp;
+        <span class="number-line" style="font-weight: 600;">
+          42
+        </span>
+        &nbsp;
         <span style="color: #cc33ff;font-weight: 600;">
           }
         </span>
