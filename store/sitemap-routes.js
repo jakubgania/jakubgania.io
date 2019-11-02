c3b880a0c550f05b3efb845f05b8730dc938f0e3
jakubgania <jakubgania@outlook.com>
2019-11-02 16:46:37 +0100

    hotfix - sitemap-routes.js

--- store/sitemap-routes.js
+++ store/sitemap-routes.js
@@ -2,61 +2,61 @@ const data = [
   {
     url: '/',
     changefreq: 'daily',
-    priority: '1',
+    priority: 1,
     lastmodISO: '2019-10-11'
   },
   {
     url: '/menu',
     changefreq: 'daily',
-    priority: '0.8',
+    priority: 0.8,
     lastmodISO: '2019-10-11'
   },
   {
     url: '/about',
     changefreq: 'daily',
-    priority: '1',
+    priority: 1,
     lastmodISO: '2019-10-11'
   },
   {
     url: '/projects',
     changefreq: 'daily',
-    priority: '0.8',
+    priority: 0.8,
     lastmodISO: '2019-10-11'
   },
   {
     url: '/contact',
     changefreq: 'daily',
-    priority: '0.5',
+    priority: 0.5,
     lastmodISO: '2019-10-11'
   },
   {
     url: '/resources',
     changefreq: 'daily',
-    priority: '0.5',
+    priority: 0.5,
     lastmodISO: '2019-10-11'
   },
   {
     url: '/blog',
     changefreq: 'daily',
-    priority: '1',
+    priority: 1,
     lastmodISO: '2019-10-11'
   },
   {
     url: '/commands',
     changefreq: 'daily',
-    priority: '0.5',
+    priority: 0.5,
     lastmodISO: '2019-10-11'
   },
   {
     url: '/shortcuts',
     changefreq: 'daily',
-    priority: '0.5',
+    priority: 0.5,
     lastmodISO: '2019-10-11'
   },
   {
     url: '/gallery',
     changefreq: 'daily',
-    priority: '0.5',
+    priority: 0.5,
     lastmodISO: '2019-10-11'
   }
 ]
