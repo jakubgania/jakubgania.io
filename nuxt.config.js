688552784a0045c4a0733872d46ea0af321a1242
jakubgania <jakubgania@outlook.com>
2019-11-05 15:29:25 +0100

    updated link section (favicon) on nuxt.config.js file

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,7 +55,13 @@ export default {
     ],
     link: [
       {
-        rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'
+        rel: 'icon', type: 'image/png', href: '/favicon-32x32.png', sizes: '32x32'
+      },
+      {
+        rel: 'icon', type: 'image/png', href: '/favicon-16x16.png', sizes: '16x16'
+      },
+      {
+        rel: 'icon', type: 'image/png', href: '/favicon.png'
       }
     ]
   },
