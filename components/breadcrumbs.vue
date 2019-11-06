787664de745241b670e2e7a0adc870eabd0855c9
jakubgania <jakubgania@outlook.com>
2019-11-06 12:27:26 +0100

    updated breadcrumbs component

--- components/breadcrumbs.vue
+++ components/breadcrumbs.vue
@@ -1,10 +1,6 @@
 <template>
-  <div>
-    <v-breadcrumbs
-      :items="items"
-      divider="/"
-      style="padding-left: 0;letter-spacing: 1px;"
-    />
+  <div id="breadcrumbs">
+    <v-breadcrumbs :items="items" divider="/" class="breadcrumbs" />
   </div>
 </template>
 
@@ -26,3 +22,13 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+#breadcrumbs .breadcrumbs {
+  padding-left: 0;
+  letter-spacing: 1px;
+}
+.v-application a {
+  color: #0066ff;
+}
+</style>
