d469c617e3b81f4181756a97839df26974f88cd7
jakubgania <jakubgania@outlook.com>
2019-11-06 11:49:11 +0100

    updated sitemap-routes file

--- store/sitemap-routes.js
+++ store/sitemap-routes.js
@@ -3,61 +3,67 @@ const data = [
     url: '/',
     changefreq: 'daily',
     priority: 1,
-    lastmodISO: '2019-10-11'
+    lastmodISO: '2019-11-06'
   },
   {
     url: '/menu',
     changefreq: 'daily',
     priority: 0.8,
-    lastmodISO: '2019-10-11'
+    lastmodISO: '2019-11-06'
   },
   {
     url: '/about',
     changefreq: 'daily',
     priority: 1,
-    lastmodISO: '2019-10-11'
+    lastmodISO: '2019-11-06'
   },
   {
     url: '/projects',
     changefreq: 'daily',
     priority: 0.8,
-    lastmodISO: '2019-10-11'
+    lastmodISO: '2019-11-06'
   },
   {
     url: '/contact',
     changefreq: 'daily',
     priority: 0.5,
-    lastmodISO: '2019-10-11'
+    lastmodISO: '2019-11-06'
   },
   {
     url: '/resources',
     changefreq: 'daily',
     priority: 0.5,
-    lastmodISO: '2019-10-11'
+    lastmodISO: '2019-11-06'
   },
   {
     url: '/blog',
     changefreq: 'daily',
     priority: 1,
-    lastmodISO: '2019-10-11'
+    lastmodISO: '2019-11-06'
   },
   {
     url: '/commands',
     changefreq: 'daily',
     priority: 0.5,
-    lastmodISO: '2019-10-11'
+    lastmodISO: '2019-11-06'
   },
   {
     url: '/shortcuts',
     changefreq: 'daily',
     priority: 0.5,
-    lastmodISO: '2019-10-11'
+    lastmodISO: '2019-11-06'
   },
   {
     url: '/gallery',
     changefreq: 'daily',
     priority: 0.5,
-    lastmodISO: '2019-10-11'
+    lastmodISO: '2019-11-06'
+  },
+  {
+    url: '/timeline',
+    changefreq: 'daily',
+    priority: 0.5,
+    lastmodISO: '2019-11-06'
   }
 ]
 
