a97e147becf6422356b5d3f235294391c4475d58
jakubgania <jakubgania@outlook.com>
2019-11-14 23:36:33 +0100

    implemented expirience component

--- pages/expirience/index.vue
+++ pages/expirience/index.vue
@@ -1,6 +1,33 @@
 <template>
   <v-row class="content-container">
     <div class="expirience-container">
+      <div class="x11" style="display: flex;">
+        <div
+          data-aos="fade-right"
+          data-aos-dealy="800"
+          data-aos-duration="1200"
+          data-aos-once="true"
+        >
+          <span class="bracket">[</span>
+        </div>
+        <div
+          data-aos="zoom-in"
+          data-aos-dealy="400"
+          data-aos-duration="1200"
+          data-aos-once="true"
+          style="width: 100%;"
+        >
+          Expirience
+        </div>
+        <div
+          data-aos="fade-left"
+          data-aos-dealy="800"
+          data-aos-duration="1200"
+          data-aos-once="true"
+        >
+          <span class="bracket">]</span>
+        </div>
+      </div>
       <div class="top-section-container">
         <client-only>
           <div v-if="$vuetify.breakpoint.name == 'xs'">
@@ -9,14 +36,13 @@
                 <img :src="profileImage" alt="" class="profile-image" />
               </div>
               <div class="full-name">
-                {{ sectionPersonalData.fullName }}
+                {{ fullName }}
               </div>
             </div>
             <div class="two-column">
               <div class="left-column">
                 <div
-                  v-for="item in sectionPersonalData.additionalInformation
-                    .leftColumn"
+                  v-for="item in personalData.additionalInformation.leftColumn"
                   :key="item.id"
                   class="item-section"
                 >
@@ -39,8 +65,7 @@
               </div>
               <div class="right-column">
                 <div
-                  v-for="item in sectionPersonalData.additionalInformation
-                    .rightColumn"
+                  v-for="item in personalData.additionalInformation.rightColumn"
                   :key="item.id"
                   class="item-section"
                 >
@@ -69,12 +94,12 @@
             </div>
             <div class="personal-data-section">
               <div class="full-name">
-                {{ sectionPersonalData.fullName }}
+                {{ fullName }}
               </div>
               <div class="two-column">
                 <div class="left-column">
                   <div
-                    v-for="item in sectionPersonalData.additionalInformation
+                    v-for="item in personalData.additionalInformation
                       .leftColumn"
                     :key="item.id"
                     class="item-section"
@@ -98,7 +123,7 @@
                 </div>
                 <div class="right-column">
                   <div
-                    v-for="item in sectionPersonalData.additionalInformation
+                    v-for="item in personalData.additionalInformation
                       .rightColumn"
                     :key="item.id"
                     class="item-section"
@@ -128,21 +153,21 @@
       <div class="information-section">
         <div class="main-description">
           <div>
-            {{ myDescription }}
+            {{ description }}
           </div>
         </div>
-        <template v-for="item in data">
+        <template v-for="item in information">
           <div :key="item.id" class="head-section">
             <h2 class="head-title">
               {{ item.titleSection }}
             </h2>
           </div>
           <div :key="item.id" class="color-line" />
-          <template v-for="(itemSection, index) in item.sectionData">
+          <template v-for="(itemSection, index2) in item.sectionData">
             <div
               :key="itemSection.id"
               class="section"
-              :class="{ 'border-bottom': index < item.sectionData.length - 1 }"
+              :class="{ 'border-bottom': index2 < item.sectionData.length - 1 }"
             >
               <div class="title-section">
                 <div class="title-text">
@@ -157,6 +182,26 @@
                   {{ itemSection.subtitle }}
                 </div>
                 <div class="description" v-html="itemSection.description" />
+                <div v-if="itemSection.icons" class="images-container">
+                  <div
+                    v-for="imageItem in itemSection.icons"
+                    :key="imageItem.id"
+                    class="image-tile"
+                  >
+                    <div class="image-section">
+                      <img
+                        :src="
+                          `https://jakubgania.io/data/expirience/technology-icons/${imageItem.path}`
+                        "
+                        alt=""
+                        class="xghp"
+                      />
+                    </div>
+                    <div class="image-description">
+                      {{ imageItem.description }}
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </template>
--- pages/expirience/script.js
+++ pages/expirience/script.js
@@ -1,15 +1,32 @@
+import axios from 'axios'
 import profileImage from '../../assets/images/profile.jpg'
 
 export default {
   data() {
     return {
-      profileImage,
-      myDescription:
-        'Pasjonują mnie technologie webowe i cały czas poszerzam więdzę w tym zakresie (zarówno z frontendu jak i backendu). Posiadam już pierwsze doświadczenie zawodowe. Poza pracą, stale realizuję swoje projekty oraz uczęszczam na branżowe meetupy. Jestem otwarty na nowe wyzwania. Zachęcam do zapoznania z moim profilem na platformie Github.'
+      profileImage
     }
   },
   mounted() {
-    console.log(this.data)
+    // console.log(this.data)
+  },
+  asyncData({ params, error }) {
+    return axios
+      .get(`https://jakubgania.io/data/expirience/data.json`)
+      .then((res) => {
+        console.log(res)
+        return {
+          personalData: res.data.expirience.sectionPersonalData,
+          fullName: res.data.expirience.sectionPersonalData.fullName,
+          description: res.data.expirience.sectionPersonalData.description,
+          information: res.data.expirience.information
+        }
+      })
+      .catch(() => {
+        return {
+          error: true
+        }
+      })
   },
   head() {
     return {
