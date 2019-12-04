ad625fadd564175b7ac401d1a2f0cf2cac3b2f4b
jakubgania <jakubgania@outlook.com>
2019-12-04 02:15:38 +0100

    hotfix for index page - aos

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -6,27 +6,26 @@
         data-aos-dealy="200"
         data-aos-duration="1200"
         data-aos-once="true"
-        class="section"
       >
-        <div class="company-name-section">
+        <div class="section">
           <logo-component :big-font-size="true" :dark-theme="darkTheme" />
-        </div>
-        <div class="link-section">
-          <nuxt-link to="/menu" class="link">
-            <span style="color: #ff0000;">
-              this
-            </span>
-            <span style="color: #000000;">
-              .
-            </span>
-            <span style="color: #0066ff;">
-              showMenu
-            </span>
-            <span style="color: #cc33ff;">
-              ()
-            </span>
-            <span>;</span>
-          </nuxt-link>
+          <div class="link-section">
+            <nuxt-link to="/menu" class="link">
+              <span style="color: #ff0000;">
+                this
+              </span>
+              <span style="color: #000000;">
+                .
+              </span>
+              <span style="color: #0066ff;">
+                showMenu
+              </span>
+              <span style="color: #cc33ff;">
+                ()
+              </span>
+              <span>;</span>
+            </nuxt-link>
+          </div>
         </div>
       </div>
     </div>
