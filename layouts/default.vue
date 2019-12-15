278602d7dc24564e0eb78c498f9b80e080e57f2f
jakubgania <jakubgania@outlook.com>
2019-12-15 19:30:49 +0100

    implemented preloader

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,21 @@
 <template>
   <v-app>
+    <div class="pre-loader-fullscreen" :class="{ 'pre-loader': loader }">
+      <div class="loader-section">
+        <div class="loader">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+
     <client-only>
       <portal-rules-alert-component />
     </client-only>
@@ -40,11 +56,15 @@ export default {
   },
   data() {
     return {
-      drawer: false
+      drawer: false,
+      loader: false
     }
   },
   computed: {
     ...mapGetters('DarkMode', ['darkTheme'])
+  },
+  mounted() {
+    this.loader = true
   },
   methods: {
     switchNavigationDrawer() {
@@ -59,6 +79,91 @@ export default {
 
 <style lang="scss">
 @import '../assets/styles/fonts.scss';
+
+.pre-loader-fullscreen {
+  background-color: white;
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  z-index: 9999;
+}
+.pre-loader {
+  display: none;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  outline: none;
+}
+
+.loader-section {
+  font-size: 16px;
+  font-smooth: auto;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+}
+
+.loader {
+  position: relative;
+  display: grid;
+  grid-template-columns: 8% 8% 8%;
+  grid-gap: 38px;
+  width: 100px;
+  height: 100px;
+
+  > div {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    background: black;
+    transform: scale(0);
+    transform-origin: center center;
+    animation: loader 2s infinite linear;
+
+    // &:nth-of-type(7) {}
+
+    &:nth-of-type(1),
+    &:nth-of-type(5),
+    &:nth-of-type(9) {
+      animation-delay: 0.4s;
+    }
+
+    &:nth-of-type(4),
+    &:nth-of-type(8) {
+      animation-delay: 0.2s;
+    }
+
+    &:nth-of-type(2),
+    &:nth-of-type(6) {
+      animation-delay: 0.6s;
+    }
+
+    &:nth-of-type(3) {
+      animation-delay: 0.8s;
+    }
+  }
+}
+
+@keyframes loader {
+  0% {
+    transform: scale(0);
+  }
+  40% {
+    transform: scale(1);
+  }
+  80% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(0);
+  }
+}
 
 html {
   scroll-behavior: smooth;
@@ -96,6 +201,12 @@ $scrollbar-thumb-color: #b3b3b3;
 @media only screen and (max-width: 600px) {
   .container-nuxt {
     padding: unset;
+  }
+  .loader {
+    grid-template-columns: 11% 11% 11%;
+    grid-gap: 27px;
+    width: 80px;
+    height: 80px;
   }
 }
 </style>
