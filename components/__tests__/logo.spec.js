4e4c8ba74f706ca2916919cd92b3d71f9a2a81a3
jakubgania <contact@jakubgania.io>
2020-02-09 22:57:41 +0100

    updated

--- components/__tests__/logo.spec.js
+++ components/__tests__/logo.spec.js
@@ -1,19 +1,51 @@
 import { shallowMount } from "@vue/test-utils";
 import logo from "../logo";
 
-const factory = () => {
-    return shallowMount(logo, {
-    });
-  };
-
-  describe("Logo", () => {
-    test("mounts properly", () => {
-      const wrapper = factory();
-      expect(wrapper.isVueInstance()).toBeTruthy();
-    });
-  
-    // test("renders properly", () => {
-    //   const wrapper = factory();
-    //   expect(wrapper.html()).toMatchSnapshot();
-    // });
+const factory = (propsData) => {
+  return shallowMount(logo, {
+    propsData: {
+      ...propsData
+    }
   });
+};
+
+describe("Logo", () => {
+test("mounts properly", () => {
+    const wrapper = factory();
+    expect(wrapper.isVueInstance()).toBeTruthy();
+  });
+
+test("example", () => {
+    const wrapper = factory({
+      darkTheme: true,
+      bigFontSize: true,
+      normalFontSize: true
+    });
+
+    console.log(wrapper.vm.$props.darkTheme);
+
+    expect(wrapper.isVueInstance()).toBeTruthy();
+  });
+
+  test("example 2", () => {
+    const wrapper = factory({
+      darkTheme: false,
+      bigFontSize: false,
+      normalFontSize: false
+    });
+
+    console.log(wrapper.vm.$props.darkTheme);
+
+    expect(wrapper.isVueInstance()).toBeTruthy();
+  });
+  
+// it('render', () => {
+//     const wrapper = factory({
+//       darkTheme: true,
+//       bigFontSize: true,
+//       normalFontSize: true
+//     });
+
+//     expect(wrapper.html()).to.equal('<div class="logo logo--normal-font">Jakub Gania Software</div>')
+//   });
+});
