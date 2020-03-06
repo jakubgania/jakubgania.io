434b8303d5680f7df7d72a30f0e1b598bb5c93fc
jakubgania <contact@jakubgania.io>
2020-03-07 00:29:00 +0100

    updated about-creator component

--- components/about-creator.vue
+++ components/about-creator.vue
@@ -14,7 +14,18 @@
           {{ description }}
         </div>
         <div>
-          social component
+          <a
+            v-for="item in socialLinks"
+            :key="item.id"
+            :href="item.link"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="social-icon"
+          >
+            <v-icon :class="{ 'icon-dark-theme': darkTheme }" class="icon">
+              {{ item.icon }}
+            </v-icon>
+          </a>
         </div>
       </div>
     </div>
@@ -22,6 +33,7 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import profileImage from './../assets/images/profile-min.jpg'
 
 export default {
@@ -30,8 +42,29 @@ export default {
       profileImage,
       fullname: 'Jakub Gania',
       description:
-        'Kiedy tylko przyjdzie mi jakiś temat do głowy to staram się to opisać w formie postu na moim blogu. Zapraszam do zapoznania się z innymi postami oraz całą moją stroną. Staram się być również aktywny na Githubie i tworzyć różne projekty.'
+        'Kiedy tylko przyjdzie mi jakiś temat do głowy to staram się to opisać w formie postu na moim blogu. Zapraszam do zapoznania się z innymi postami oraz całą moją stroną. Staram się być również aktywny na Githubie i tworzyć różne projekty.',
+      socialLinks: [
+        {
+          link: 'https://github.com/jakubgania',
+          icon: 'mdi-github-circle'
+        },
+        {
+          link: 'https://www.linkedin.com/in/jakubgania/',
+          icon: 'mdi-linkedin'
+        },
+        {
+          link: 'https://twitter.com/jakubgania',
+          icon: 'mdi-twitter'
+        },
+        {
+          link: 'https://www.youtube.com/channel/UCpRXjQ_FgRfAqP6uIsA7UEQ',
+          icon: 'mdi-youtube'
+        }
+      ]
     }
+  },
+  computed: {
+    ...mapGetters('DarkMode', ['darkTheme'])
   }
 }
 </script>
@@ -45,13 +78,13 @@ export default {
 }
 .author-wrapper {
   display: flex;
+  margin-left: 10px;
+  margin-right: 10px;
 }
 .author-image {
   width: 100px;
-  // background-color: red;
 }
 .author-description-section {
-  // background-color: blue;
   width: calc(100% - 100px);
 }
 .author-fullname {
@@ -61,5 +94,23 @@ export default {
 }
 .author-description {
   font-size: 16px;
+  margin-bottom: 10px;
+}
+.social-icon {
+  margin-right: 10px;
+  text-decoration: none;
+}
+.icon {
+  color: #000;
+}
+.icon-dark-theme {
+  color: #fff;
+}
+
+@media only screen and (max-width: 600px) {
+  .author-wrapper {
+    margin-left: 10px;
+    margin-right: 10px;
+  }
 }
 </style>
